9dd5a19c0cd62c77d8c44a9efc4d60583cbe72db
maaydebrabot <127371100+maaydebrabot@users.noreply.github.com>
2023-03-09 00:15:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ say('Mystic - Bot\nWhatsApp Bot MD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Bot creado por Bruno Sobrino`, {
+say(`BOT CREADO POR MAIIDE`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
